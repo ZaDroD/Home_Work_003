2a44f7671ff28c5304ee77dea9b75cd558b131f2
Evgeniy <zadrod93@yandex.ru>
2022-02-13 13:30:39 +0300

    Задача 22

---
+++ Example22/Program.cs
@@ -0,0 +1,47 @@
+﻿// 22 Найти расстояние между точками в пространстве 2D / 3D
+
+double Distance2D(double x1, double y1, double x2, double y2)
+{
+    double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+    return result;
+}
+
+double Distance3D(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+    return result;
+}
+
+Console.Write("Выберите пространство для поиска расстояний между точками, 2D или 3D : ");
+string choise = Console.ReadLine();
+
+if (choise == "2D")
+{
+    Console.Write("Введите координату x1 ");
+    double x1 = double.Parse(Console.ReadLine());
+    Console.Write("Введите координату y1 ");
+    double y1 = double.Parse(Console.ReadLine());
+    Console.Write("Введите координату x2 ");
+    double x2 = double.Parse(Console.ReadLine());
+    Console.Write("Введите координату y2 ");
+    double y2 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Расстояние между точками = " + Distance2D(x1, y1, x2, y2) + ".");
+}
+else if (choise == "3D")
+{
+    Console.Write("Введите координату x1 ");
+    double x1 = double.Parse(Console.ReadLine());
+    Console.Write("Введите координату y1 ");
+    double y1 = double.Parse(Console.ReadLine());
+    Console.Write("Введите координату z1 ");
+    double z1 = double.Parse(Console.ReadLine());
+    Console.Write("Введите координату x2 ");
+    double x2 = double.Parse(Console.ReadLine());
+    Console.Write("Введите координату y2 ");
+    double y2 = double.Parse(Console.ReadLine());
+    Console.Write("Введите координату z2 ");
+    double z2 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Расстояние между точками = " + Distance3D(x1, y1, z1, x2, y2, z2) + ".");
+}
+
+else Console.Write("Введен неправильный запрос");
