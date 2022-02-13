944d859e052938e36ffc431a7d65bf20451ebaba
Evgeniy <zadrod93@yandex.ru>
2022-02-13 13:32:27 +0300

    Задача 24

---
+++ Example24/Program.cs
@@ -0,0 +1,12 @@
+﻿// 24 Найти кубы чисел от 1 до N
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int n = 1;
+
+while (n <= num)
+{
+    Console.Write(n* n* n);
+    n ++;
+}
